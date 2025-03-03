4c92c3645bc77423cc91b143e9e5fd0bae498bc8
Reanna Lofamia <rplofamia@student.apc.edu.ph>
2025-03-03 12:36:23 +0000

    Fixed asset loading issue in Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 👈 Add this line
   plugins: [
     vue(),
     vueDevTools(),
