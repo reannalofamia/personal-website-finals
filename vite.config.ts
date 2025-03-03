8f390ea9e48fd115d6c853d02896be008390f570
Reanna Lofamia <rplofamia@student.apc.edu.ph>
2025-03-03 14:02:09 +0000

    Fix font path issue in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './', // 👈 Add this line
+  base: './',
+  publicDir: 'public', // 👈 Ensures Vite correctly serves static files
   plugins: [
     vue(),
     vueDevTools(),
